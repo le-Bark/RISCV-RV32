90852fff63193a76ad2c75ee525ce2f6bb34d2cd
Angelo B-Gomez <angelo.bautista.g@gmail.com>
2020-07-28 14:24:46 -0400

    data_mem store and load debug

--- data_memory.vhd
+++ data_memory.vhd
@@ -32,41 +32,53 @@ use ieee.math_real.all;
 --use UNISIM.VComponents.all;
 
 entity data_memory is
-  Port ( 
-     clk     : in std_logic;
-     reset : in std_logic;
-     address :  in std_logic_vector(31 downto 0);
-     write_data : in std_logic_vector(31 downto 0);
-     mem_read : in std_logic;
-     mem_write : in std_logic;
-     read_data : out std_logic_vector(31 downto 0)
+  Port (
+    clk        : in  std_logic;
+    reset      : in  std_logic;
+    address    : in  std_logic_vector(31 downto 0);
+    write_data : in  std_logic_vector(31 downto 0);
+    mem_read   : in  std_logic;
+    store_ctrl : in  std_logic_vector(2 downto 0);
+    mem_write  : in  std_logic;
+    read_data  : out std_logic_vector(31 downto 0)
   );
 end data_memory;
 
 architecture mem of data_memory is
 
-constant MEM_SIZE : integer := 1024;
-constant ADDR_SIZE : integer := integer(ceil(log2(real(MEM_SIZE))));
-type ram_type is array (MEM_SIZE-1 downto 0) of std_logic_vector(31 downto 0);
-signal data_mem : ram_type;
-
+  constant MEM_SIZE  : integer := 4096;
+  constant ADDR_SIZE : integer := integer(ceil(log2(real(MEM_SIZE))));
+  type ram_type is array (0 to MEM_SIZE-1) of std_logic_vector(7 downto 0);
+  signal data_mem    : ram_type;
+  signal int_add : integer:= to_integer(unsigned(address(ADDR_SIZE-1 downto 0)));  
+ 
 
 begin
 
-process(clk) is 
-begin
-if rising_edge(clk) then
-    if reset = '1' then
+  process(clk) is
+  begin
+    if rising_edge(clk) then
+      if reset = '1' then
         for i in 0 to MEM_SIZE - 1 loop
           data_mem(i) <= (others => '0');
         end loop;
-    elsif mem_write = '1' then
-        data_mem(to_integer(unsigned(address(ADDR_SIZE-1 downto 0)))) <= write_data;
-    end if;
- end if; 
-end process;
+      elsif mem_write = '1' then
+        case store_ctrl is
 
-read_data <=  data_mem(to_integer(unsigned(address(ADDR_SIZE-1 downto 0)))) when mem_read = '1' else
-              (others => '0');
+          when "000" => data_mem(int_add) <= write_data(7 downto 0); -- Store Byte
+          when "001" => data_mem(int_add) <= write_data(7 downto 0); --Store 16bits
+                        data_mem(int_add +1) <= write_data(15 downto 8);
+          when others =>  data_mem(int_add) <= write_data(7 downto 0); --Store 32bits
+                          data_mem(int_add +1) <= write_data(15 downto 8);
+                          data_mem(int_add +2) <= write_data(23 downto 16);
+                          data_mem(int_add +3) <= write_data(31 downto 24);
+        end case;
+       elsif mem_read = '1' then
+       read_data <= data_mem(int_add + 3) & data_mem(int_add + 2) & data_mem(int_add +1) & data_mem(int_add); 
+      end if;
+    end if;
+  end process;
+
+  
 
 end mem;
